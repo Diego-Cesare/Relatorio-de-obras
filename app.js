e0323eb2fad4e63e24fe5980db94e261be0131e3
Diego Cesare <diegocesare300491@gmail.com>
2026-02-17 22:32:34 -0300

    Adicionar opção de incluir mais imagens

--- app.js
+++ app.js
@@ -13,6 +13,60 @@ const previewImagemEl = document.getElementById("previewImagem");
 // botão de compartilhar
 const btnShare = document.getElementById("btnShare");
 
+const localizacao = document.getElementById("geolocateBtn");
+
+async function obterEndereco() {
+  if (!navigator.geolocation) {
+    throw new Error("Geolocalização não suportada pelo navegador.");
+  }
+
+  // Função para obter posição como Promise
+  function posicaoAtual() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  }
+
+  try {
+    const position = await posicaoAtual();
+    const { latitude, longitude } = position.coords;
+
+    // Requisição para OpenStreetMap (reverse geocoding)
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar endereço.");
+    }
+
+    const data = await response.json();
+
+    return {
+      bairro: data.address.suburb || data.address.neighbourhood || "Não encontrado",
+      rua: data.address.road || "Não encontrada"
+    };
+
+  } catch (error) {
+    console.error("Erro:", error);
+    throw error;
+  }
+}
+
+
+const locBairro = document.getElementById("bairro");
+const locRua = document.getElementById("rua");
+localizacao.addEventListener("click", () => {
+obterEndereco()
+  .then(endereco => {
+    locBairro.value = endereco.bairro;
+    locRua.value = endereco.rua;
+  })
+  .catch(err => {
+    console.error("Falha ao obter endereço:", err);
+  });
+})
+
 // inicia com div de imagem null
 let imagemSelecionada = null;
 let previewUrl = "";
